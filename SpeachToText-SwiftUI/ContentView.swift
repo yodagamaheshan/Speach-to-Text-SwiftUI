0936a67128923984002ea32dbe3cac843a3805bf
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-03-23 15:41:58 +0530

    Initial Commit

---
+++ SpeachToText-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeachToText-SwiftUI
+//
+//  Created by Heshan Yodagama on 3/23/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
