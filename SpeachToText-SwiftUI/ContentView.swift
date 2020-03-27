325df90703f5312b7acf876c4dd542176d6c559e
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-03-27 06:28:50 +0530

    add recording button

--- SpeachToText-SwiftUI/ContentView.swift
+++ SpeachToText-SwiftUI/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            Button(action: {
+                //recording start
+            }) {
+                HStack {
+                    Image(systemName: "mic.fill")
+                    Text("Record")
+                }
+            }
+        }
     }
 }
 
